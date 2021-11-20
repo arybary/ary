3d988d4019570a597e408be1985b803199b5455b
arybary <l3fljl5ldydy@gmail.com>
2021-11-20 14:51:49 +0200

    ary

---
+++ temp/index.js
@@ -0,0 +1,12 @@
+const superRound = (num, prec) => {
+  const n = 10 ** prec;
+  
+  return [  
+    Math.floor(num * n) / n,
+    Math.trunc(num * n) / n,
+    Math.ceil(num * n) / n,
+    Math.round(num * n) / n,
+    +num.toFixed(prec),
+  ];
+
+};
